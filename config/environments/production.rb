d1ff15f645a973e5d07c0e349323d5e2c4a77e33
yamaguchi1011 <yamaguchi1001@icloud.com>
2020-09-10 23:45:22 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,16 +5,16 @@ Rails.application.configure do
   config.cache_classes = true
   
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivary_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
  
