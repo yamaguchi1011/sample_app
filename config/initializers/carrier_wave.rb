290a0a0f7ac51f912429dc2460d85980251c528d
yamaguchi1011 <yamaguchi1001@icloud.com>
2020-09-27 05:14:16 +0000

    Add user following

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -17,12 +17,9 @@ CarrierWave.configure do |config|
     # iam_profile
     config.fog_credentials = {
       provider: 'AWS',
-      # credentialsで管理する場合
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       # 環境変数で管理する場合
-      # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1' #東京リージョン
     }
     # キャッシュをS3に保存
